8155139b95be9ebea2331c2bec344e12d499c8cd
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-11 12:55:31 +0100

    Add functionnality of burger menu with javascript

---
+++ src/assets/js/home.js
@@ -0,0 +1,11 @@
+const body = document.getElementsByTagName("body");
+
+const iconMenu = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+
+iconMenu.onclick = () => {
+    // add class 'noScroll' to body to stop scrolling when burger menu is open
+    body[0].classList.toggle("noScroll");
+    menu.classList.toggle("drop-menu");
+};
+
