31b3ac64de5765b8ed993e084bcdb0115112b06f
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-11-11 22:19:52 +0100

    List cards

--- src/assets/js/promotion.js
+++ src/assets/js/promotion.js
@@ -1,20 +1,37 @@
 const grille = document.getElementById("grille");
 const block = document.getElementById("block");
 
-const list_cards = document.querySelector("#list-grille-cards");
+const list_grille_cards = document.querySelector("#list-grille-cards");
+const list_cards = document.querySelector("#liste-cards");
 
 grille.addEventListener("click", () => {
-    list_cards.style.transition = "all 0.3s ease-in-out";
-    list_cards.style.opacity = "0";
+    list_grille_cards.style.transition = "all 0.3s ease-in-out";
+    list_grille_cards.style.opacity = "0";
     setTimeout(() => {
-        list_cards.style.display = "none";
+        list_grille_cards.style.display = "none";
     }, 500);
+
+    setInterval(() => {
+        list_cards.style.transition = "all 0.3s ease-in-out";
+        list_cards.style.opacity = 1;
+        setTimeout(() => {
+            list_cards.style.display = "block";
+        }, 500);
+    }, 1000);
 })
 
 block.addEventListener("click", () => {
-    list_cards.style.transition = "all 0.3s ease-in-out";
-    list_cards.style.opacity = 1;
+    list_grille_cards.style.transition = "all 0.3s ease-in-out";
+    list_grille_cards.style.opacity = 1;
     setTimeout(() => {
-        list_cards.style.display = "grid";
+        list_grille_cards.style.display = "grid";
     }, 500);
+
+    setInterval(() => {
+        list_cards.style.transition = "all 0.3s ease-in-out";
+        list_cards.style.opacity = "0";
+        setTimeout(() => {
+            list_cards.style.display = "none";
+        }, 500);
+    }, 1000);
 })
