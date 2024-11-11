9d7fa9b5d876e07a66e359ed452779158af1afa1
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-11-11 22:34:20 +0100

    Ajouter fonctunalité js pour le click le list/grill

--- src/assets/js/promotion.js
+++ src/assets/js/promotion.js
@@ -5,19 +5,20 @@ const list_grille_cards = document.querySelector("#list-grille-cards");
 const list_cards = document.querySelector("#liste-cards");
 
 grille.addEventListener("click", () => {
+
     list_grille_cards.style.transition = "all 0.3s ease-in-out";
     list_grille_cards.style.opacity = "0";
     setTimeout(() => {
         list_grille_cards.style.display = "none";
-    }, 500);
+    }, 300);
 
-    setInterval(() => {
+    setTimeout(() => {
         list_cards.style.transition = "all 0.3s ease-in-out";
-        list_cards.style.opacity = 1;
+        list_cards.style.opacity = "1";
         setTimeout(() => {
-            list_cards.style.display = "block";
+            list_cards.style.display = "grid";
         }, 500);
-    }, 1000);
+    }, 300);
 })
 
 block.addEventListener("click", () => {
@@ -27,11 +28,11 @@ block.addEventListener("click", () => {
         list_grille_cards.style.display = "grid";
     }, 500);
 
-    setInterval(() => {
+    setTimeout(() => {
         list_cards.style.transition = "all 0.3s ease-in-out";
         list_cards.style.opacity = "0";
         setTimeout(() => {
             list_cards.style.display = "none";
         }, 500);
-    }, 1000);
+    }, 500);
 })
