10104c886e4f9700e9675c2f1ccc48be659d616c
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-11 12:57:08 +0100

    Make buttons in slider work, also add function to auto-slide each 5s

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -9,3 +9,27 @@ iconMenu.onclick = () => {
     menu.classList.toggle("drop-menu");
 };
 
+// import json ads data from its path
+import carouselData from "../data/images-carousel.json" with {type: "json"};
+const mainImage = document.getElementById("image-carousel");
+const chevronLeft = document.getElementById("chevron-left");
+const chevronRight = document.getElementById("chevron-right");
+
+mainImage.src = carouselData[0].url;
+let index = 0;
+chevronRight.onclick = function nextImage(){
+    index++;
+    if(index == 5) index = 0;
+    mainImage.src = carouselData[index].url;
+}
+chevronLeft.onclick = function previousImage(){
+    index--;
+    if(index == -1) index = carouselData.length - 1;
+    mainImage.src = carouselData[index].url;
+}
+// make images slide itself each 5s
+setInterval(() => {
+    index++;
+    if(index == 5) index = 0;
+    mainImage.src = carouselData[index].url;
+}, 5000);
