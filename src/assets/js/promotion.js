e751ebd7ce80aee6641c5d68f8bd4362c04efc9d
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-11-12 17:26:05 +0100

    Remplacer les element statique (card/grille) par des data proviennent d'un API json server

--- src/assets/js/promotion.js
+++ src/assets/js/promotion.js
@@ -7,50 +7,100 @@ const list_cards = document.querySelector("#liste-cards");
 const loader = document.querySelector("#loader");
 
 grille.addEventListener("click", () => {
+  list_grille_cards.style.transition = "all 0.3s ease-in-out";
+  list_grille_cards.style.opacity = "0";
 
-    list_grille_cards.style.transition = "all 0.3s ease-in-out";
-    list_grille_cards.style.opacity = "0";
+  list_cards.style.transition = "all 0.3s ease-in-out";
+  list_cards.style.opacity = "1";
 
-    list_cards.style.transition = "all 0.3s ease-in-out";
-    list_cards.style.opacity = "1";
+  loader.style.display = "block";
 
-    loader.style.display = "block"
+  setTimeout(() => {
+    list_grille_cards.style.display = "none";
+    loader.style.display = "none";
+  }, 1000);
 
-    setTimeout(() => {
-        list_grille_cards.style.display = "none";
-        loader.style.display = "none"
-    }, 1000);
-
-    setTimeout(() => {
-        list_cards.style.display = "grid";
-    }, 1000);
-})
+  setTimeout(() => {
+    list_cards.style.display = "grid";
+  }, 1000);
+});
 
 block.addEventListener("click", () => {
-    list_grille_cards.style.transition = "all 0.3s ease-in-out";
-    list_grille_cards.style.opacity = 1;
+  list_grille_cards.style.transition = "all 0.3s ease-in-out";
+  list_grille_cards.style.opacity = 1;
 
-    list_cards.style.transition = "all 0.3s ease-in-out";
-    list_cards.style.opacity = "0";
+  list_cards.style.transition = "all 0.3s ease-in-out";
+  list_cards.style.opacity = "0";
 
-    loader.style.display = "block"
+  loader.style.display = "block";
 
-    setTimeout(() => {
-        list_grille_cards.style.display = "grid";
-        loader.style.display = "none"
-    }, 1000);
+  setTimeout(() => {
+    list_grille_cards.style.display = "grid";
+    loader.style.display = "none";
+  }, 1000);
 
-    setTimeout(() => {
-        list_cards.style.display = "none";
-    }, 1000);
+  setTimeout(() => {
+    list_cards.style.display = "none";
+  }, 1000);
 });
 
 // Consomation de API avec AXIOX
 
+
 let Products_Promo = [];
 
-axios.get("http://localhost:3000/promotions").then((res) => {
-    Products_Promo.push(...res.data)
-});
+axios.get("http://localhost:3000/promotions")
+  .then(response => {
+    Products_Promo = response.data;
 
-console.table(Products_Promo);
+    Products_Promo.forEach((product) => {
+      list_grille_cards.innerHTML += `
+        <div class="promotion-card w-[200px]" id="${product.id}">
+          <div class="relative img">
+            <img src="${product.img}" alt="image de camera" class="w-[200px] h-[220px]" />
+
+            <div class="overlay"></div>
+
+            <div id="plus">+</div>
+
+            <div class="absolute top-[20px] left-[20px] content">
+              <p class="w-[60px] h-[25px] flex text-sm items-center justify-center bg-white shadow-xl">
+                ${product.category}
+              </p>
+              <p class="w-[60px] h-[25px] flex text-sm items-center justify-center bg-white mt-2 shadow-xl">
+                -${product.discount}
+              </p>
+            </div>
+          </div>
+          <p class="mt-2 text-sm text-center w-[85%] m-auto">
+            ${product.description}
+          </p>
+        </div>
+      `;
+
+      list_cards.innerHTML += `
+        <div class="flex card-list w-[73%] m-auto card-shadow my-4" id="${product.id}">
+              <div class="w-[250px] md:w-[180px] h-[140px] md:h-[160px] lg:h-[200px]">
+                <img src="${product.img}" alt="image camera"
+                  class="w-[250px] md:w-[180px] h-[140px] md:h-[160px] lg:h-[200px]" />
+              </div>
+              <div class="flex flex-col ml-10 justify-evenly">
+                <div class="flex gap-10">
+                  <span
+                    class="w-[40px] md:w-[50px] h-[20px] md:h-[25px] flex text-xs md:text-sm lg:text-lg items-center justify-center bg-white shadow-xl">${product.category}</span>
+                  <span
+                    class="w-[40px] md:w-[50px] h-[20px] md:h-[25px] flex text-xs md:text-sm lg:text-lg items-center justify-center bg-white shadow-xl">-${product.discount}</span>
+                </div>
+                <p class="text-xs font-bold md:text-sm lg:text-lg">${product.description}</p>
+                <div
+                  class="w-[100px] lg:w-[150px] h-[2rem] lg:h-[3rem] text-sm lg:text-lg flex items-center justify-center font-bold bg-white shadow-xl">
+                  Order Now
+                </div>
+              </div>
+            </div>
+      `;
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
