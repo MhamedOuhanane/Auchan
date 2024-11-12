9944c36027998c7ab8f43ffe895f81b10edf0755
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-11-12 12:27:19 +0100

    Terminer l'animation de section de list/grille avec un loader qui afficher dans lequelle en click buttons list/grille

--- src/assets/js/promotion.js
+++ src/assets/js/promotion.js
@@ -4,35 +4,43 @@ const block = document.getElementById("block");
 const list_grille_cards = document.querySelector("#list-grille-cards");
 const list_cards = document.querySelector("#liste-cards");
 
+const loader = document.querySelector("#loader");
+
 grille.addEventListener("click", () => {
 
     list_grille_cards.style.transition = "all 0.3s ease-in-out";
     list_grille_cards.style.opacity = "0";
-    setTimeout(() => {
-        list_grille_cards.style.display = "none";
-    }, 300);
+
+    list_cards.style.transition = "all 0.3s ease-in-out";
+    list_cards.style.opacity = "1";
+
+    loader.style.display = "block"
 
     setTimeout(() => {
-        list_cards.style.transition = "all 0.3s ease-in-out";
-        list_cards.style.opacity = "1";
-        setTimeout(() => {
-            list_cards.style.display = "grid";
-        }, 500);
-    }, 300);
+        list_grille_cards.style.display = "none";
+        loader.style.display = "none"
+    }, 1000);
+
+    setTimeout(() => {
+        list_cards.style.display = "grid";
+    }, 1000);
 })
 
 block.addEventListener("click", () => {
     list_grille_cards.style.transition = "all 0.3s ease-in-out";
     list_grille_cards.style.opacity = 1;
-    setTimeout(() => {
-        list_grille_cards.style.display = "grid";
-    }, 500);
+
+    list_cards.style.transition = "all 0.3s ease-in-out";
+    list_cards.style.opacity = "0";
+
+    loader.style.display = "block"
 
     setTimeout(() => {
-        list_cards.style.transition = "all 0.3s ease-in-out";
-        list_cards.style.opacity = "0";
-        setTimeout(() => {
-            list_cards.style.display = "none";
-        }, 500);
-    }, 500);
+        list_grille_cards.style.display = "grid";
+        loader.style.display = "none"
+    }, 1000);
+
+    setTimeout(() => {
+        list_cards.style.display = "none";
+    }, 1000);
 })
