bc196c9554477fe9755e7c454a8e34409f2ea406
michael-rostom <ali.rostom220@gmail.com>
2024-11-13 11:17:59 +0100

    dev: add grid/list buttons functionality

---
+++ src/assets/js/catalogue.js
@@ -0,0 +1,22 @@
+var gridBtn = document.getElementById('grid');
+var listBtn = document.getElementById('list');
+
+var gridContainer = document.getElementById('grid-container');
+var listContainer = document.getElementById('list-container');
+
+
+
+gridBtn.addEventListener('click', function(){
+    gridContainer.style.display = 'flex';
+    listContainer.style.display = 'none';
+    gridBtn.classList.add = 'active 11';
+    listBtn.classList.remove = 'active 11';
+})
+listBtn.addEventListener('click', function(){
+    gridContainer.style.display = 'none';
+    listContainer.style.display = 'flex';
+    gridBtn.classList.remove = 'active 11';
+    listBtn.classList.add = 'active 11';
+})
+
+
