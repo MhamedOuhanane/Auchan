ed46da7f1ef80136db714840e5b75cef47a199a8
michael-rostom <ali.rostom220@gmail.com>
2024-11-13 12:37:40 +0100

    front-end : add active animations for grid/style buttons (catalogue)

--- src/assets/js/catalogue.js
+++ src/assets/js/catalogue.js
@@ -1,3 +1,16 @@
+const body = document.getElementsByTagName("body");
+
+const iconMenu = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+
+iconMenu.onclick = () => {
+    // add class 'noScroll' to body to stop scrolling when burger menu is open
+    body[0].classList.toggle("noScroll");
+    menu.classList.toggle("drop-menu");
+};
+
+
+
 var gridBtn = document.getElementById('grid');
 var listBtn = document.getElementById('list');
 
@@ -9,14 +22,14 @@ var listContainer = document.getElementById('list-container');
 gridBtn.addEventListener('click', function(){
     gridContainer.style.display = 'grid';
     listContainer.style.display = 'none';
-    gridBtn.classList.add = 'active 11';
-    listBtn.classList.remove = 'active 11';
+    gridBtn.classList.add('active11');
+    listBtn.classList.remove('active11');
 })
 listBtn.addEventListener('click', function(){
     gridContainer.style.display = 'none';
     listContainer.style.display = 'flex';
-    gridBtn.classList.remove = 'active 11';
-    listBtn.classList.add = 'active 11';
+    gridBtn.classList.remove('active11');
+    listBtn.classList.add('active11');
 })
 
 
