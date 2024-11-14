884e1c22faefc40afc7105b7e508fb3699e4535a
michael-rostom <ali.rostom220@gmail.com>
2024-11-14 11:48:07 +0100

    dev fixed generate card grid function

--- src/assets/js/catalogue.js
+++ src/assets/js/catalogue.js
@@ -51,16 +51,16 @@ function displayallcategories(data){
 
 }
 
-function generateCardList(name,desc,price,img){
+function generateCardList(product){
     let div = document.createElement('div');
     div.classList = 'bg-white shadow-sm hover:shadow-md rounded-lg p-4 w-[95%] max-w-3xl flex items-center gap-10';
     div.innerHTML=`
                     <div class="w-1/4 flex justify-center">
-                <img src="${img}" alt="${name}" class="w-40 h-40 object-contain">
+                <img src="${product.img}" alt="${product.name}" class="w-40 h-40 object-contain">
               </div>
               <div class="w-3/4">
                 <div class="flex justify-between items-center text-2xl">
-                  <h2 class="font-bold">${name}</h2>
+                  <h2 class="font-bold">${product.name}</h2>
                 <div class="flex items-center space-x-1">
                     <i class="fa-regular fa-star" style="color: #FFD43B;"></i>
                     <span class="text-[#959595] font-medium">4.5</span>
@@ -68,10 +68,10 @@ function generateCardList(name,desc,price,img){
                 </div>         
                 </div>
                 <p class="text-base text-[#959595] mt-1">
-                  ${desc}
+                  ${product.desc}
                 </p>
                 <div class="flex justify-between items-center mt-4">
-                  <span class="text-2xl font-bold text-darkViolet">${price}</span>
+                  <span class="text-2xl font-bold text-darkViolet">${product.price}</span>
                   <button class="text-2xl bg-darkViolet text-white px-5 py-2 rounded-[20px]">
                     Ajoutez 
                     <i class="fa-solid fa-cart-shopping"></i>  
