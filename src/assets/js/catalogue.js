6801c733188f4f2b27831c4f3635a0f7653a47be
michael-rostom <ali.rostom220@gmail.com>
2024-11-15 19:41:17 +0100

    dev: fix hamburger menu feature src(catalogue)

--- src/assets/js/catalogue.js
+++ src/assets/js/catalogue.js
@@ -1,14 +1,3 @@
-const body = document.getElementsByTagName("body");
-
-const iconMenu = document.getElementById("menu-icon");
-const menu = document.getElementById("menu");
-
-iconMenu.onclick = () => {
-    // add class 'noScroll' to body to stop scrolling when burger menu is open
-    body[0].classList.toggle("noScroll");
-    menu.classList.toggle("drop-menu");
-};
-
 
 
 var gridBtn = document.getElementById('grid');
@@ -291,7 +280,7 @@ function generateCardList(product){
                 </div>       
               </div>
     `;
-    
+
     
     return div;
 
