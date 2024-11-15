a515d3e6b246e2ebae4adc2656a06684928e19be
michael-rostom <ali.rostom220@gmail.com>
2024-11-15 11:08:40 +0100

    dev: add pagination function for list section and display all categories(catalogue)

--- src/assets/js/catalogue.js
+++ src/assets/js/catalogue.js
@@ -13,7 +13,8 @@ iconMenu.onclick = () => {
 
 var gridBtn = document.getElementById('grid');
 var listBtn = document.getElementById('list');
-
+var gridContainer = document.getElementById('grid-container');
+var listContainer = document.getElementById('list-container');
 
 
 
@@ -34,9 +35,10 @@ listBtn.addEventListener('click', function(){
 
 async function getdata(){
     try{
-        const response = await fetch('../assets/data/Catégorie.json');
+        const response = await fetch('https://dummyjson.com/products?limit=60');
         const json = await response.json();
-        displayAllCategoriesGrid(json);
+        console.log(json);
+        displayAllCategoriesList(json);
     }
     catch(error){
         console.error(error.message);
@@ -44,21 +46,39 @@ async function getdata(){
 }
 
 
-
-function displayAllCategoriesGrid(data){
-    
+function displayAllCategoriesList(data){
+    data.products.forEach((element,index) => {
+        if(index<12){
+            listPage1.appendChild(generateCardList(element));
+        }
+        else if(index<24){
+            listPage2.appendChild(generateCardList(element));
+        }
+        else if(index<36){
+            listPage3.appendChild(generateCardList(element));
+        }
+        else if(index<48){
+            listPage4.appendChild(generateCardList(element));
+        }
+        else if(index<60){
+            listPage5.appendChild(generateCardList(element));
+        }
+    });
+    ListPagination();
 }
+
+
 
 function generateCardList(product){
     let div = document.createElement('div');
     div.classList = 'bg-white shadow-sm hover:shadow-md rounded-lg p-4 w-[95%] max-w-3xl flex items-center gap-10';
     div.innerHTML=`
                     <div class="w-1/4 flex justify-center">
-                <img src="${product.image}" alt="${product.titre}" class="w-40 h-40 object-contain">
+                <img src="${product.thumbnail}" alt="${product.title}" class="w-40 h-40 object-contain">
               </div>
               <div class="w-3/4">
                 <div class="flex justify-between items-center text-2xl">
-                  <h2 class="font-bold">${product.titre}</h2>
+                  <h2 class="font-bold">${product.title}</h2>
                 <div class="flex items-center space-x-1">
                     <i class="fa-regular fa-star" style="color: #FFD43B;"></i>
                     <span class="text-[#959595] font-medium">4.5</span>
@@ -69,7 +89,7 @@ function generateCardList(product){
                   ${product.description}
                 </p>
                 <div class="flex justify-between items-center mt-4">
-                  <span class="text-2xl font-bold text-darkViolet">${product.prix}$</span>
+                  <span class="text-2xl font-bold text-darkViolet">${product.price}$</span>
                   <button class="text-2xl bg-darkViolet text-white px-5 py-2 rounded-[20px]">
                     Ajoutez 
                     <i class="fa-solid fa-cart-shopping"></i>  
@@ -87,10 +107,10 @@ function generateCardGrid(product){
     div.classList='card shadow-sm hover:shadow-md transition-all ease-out delay-75';
     div.innerHTML = `
                         <div class="h-[140px] center">
-                <img src="${product.image}" alt="${product.titre}" class="h-full object-contain">
+                <img src="${product.thumbnail}" alt="${product.title}" class="h-full object-contain">
               </div>
               <div class="flex justify-between items-center flex-wrap">
-                <span class="font-bold text-[1.2rem] max-md:text-[1rem] max-sm:text-[.7rem]">${product.titre}</span>
+                <span class="font-bold text-[1.2rem] max-md:text-[1rem] max-sm:text-[.7rem]">${product.title}</span>
                 <div class="flex items-center gap-2 text-gray-500 max-md:text-xs">
                   <img src="assets/images/icons/star.svg" alt="star icon" class="size-5">
                   <span class="">4.5</span>
@@ -98,7 +118,7 @@ function generateCardGrid(product){
                 </div>
                 <span class="text-gray-700 max-md:text-[.7rem] max-sm:text-[.5rem] min-h-[60px] max-md:min-h-[50px]">${product.description}</span>
                 <div class="flex items-center justify-between w-full flex-wrap pt-4 gap-4">
-                  <span class="font-bold text-darkViolet text-[1.4rem] max-md:text-[1rem]">${product.prix}$</span>
+                  <span class="font-bold text-darkViolet text-[1.4rem] max-md:text-[1rem]">${product.price}$</span>
                   <button class="primary-btn center gap-2 flex-1">
                     <span class="text-[.8rem] max-sm:text-[.5rem]">AJOUTER</span>
                     <img src="assets/images/icons/cart-white.svg" class="size-4" alt="">
@@ -111,4 +131,92 @@ function generateCardGrid(product){
 
 getdata();
 
+
+function ListPagination(){
+    btn1.addEventListener('click', function(){
+        listPage1.classList.add('flex');
+        listPage1.classList.remove('hidden');
+        btn1.classList.add('active12');
+
+        listPage2.classList.add('hidden');
+        btn2.classList.remove('active12');
+
+        listPage3.classList.add('hidden');
+        btn3.classList.remove('active12');
+
+        listPage4.classList.add('hidden');
+        btn4.classList.remove('active12');
+
+        listPage5.classList.add('hidden');
+        btn5.classList.remove('active12');
+    });
+    btn2.addEventListener('click', function(){
+        listPage2.classList.add('flex');
+        listPage2.classList.remove('hidden');
+        btn2.classList.add('active12');
+
+        listPage1.classList.add('hidden');
+        btn1.classList.remove('active12');
+
+        listPage3.classList.add('hidden');
+        btn3.classList.remove('active12');
+
+        listPage4.classList.add('hidden');
+        btn4.classList.remove('active12');
+
+        listPage5.classList.add('hidden');
+        btn5.classList.remove('active12');
+    });
+    btn3.addEventListener('click', function(){
+        listPage3.classList.add('flex');
+        listPage3.classList.remove('hidden');
+        btn3.classList.add('active12');
+
+        listPage1.classList.add('hidden');
+        btn1.classList.remove('active12');
+
+        listPage2.classList.add('hidden');
+        btn2.classList.remove('active12');
+
+        listPage4.classList.add('hidden');
+        btn4.classList.remove('active12');
+
+        listPage5.classList.add('hidden');
+        btn5.classList.remove('active12');
+    });
+    btn4.addEventListener('click', function(){
+        listPage4.classList.add('flex');
+        listPage4.classList.remove('hidden');
+        btn4.classList.add('active12');
+
+        listPage1.classList.add('hidden');
+        btn1.classList.remove('active12');
+
+        listPage2.classList.add('hidden');
+        btn2.classList.remove('active12');
+        
+        listPage3.classList.add('hidden');
+        btn3.classList.remove('active12');
+        
+        listPage5.classList.add('hidden');
+        btn5.classList.remove('active12');
+    });
+    btn5.addEventListener('click', function(){
+        listPage5.classList.add('flex');
+        listPage5.classList.remove('hidden');
+        btn5.classList.add('active12');
+
+        listPage1.classList.add('hidden');
+        btn1.classList.remove('active12');
+
+        listPage2.classList.add('hidden');
+        btn2.classList.remove('active12');
+
+        listPage3.classList.add('hidden');
+        btn3.classList.remove('active12');
+
+        listPage4.classList.add('hidden');
+        btn4.classList.remove('active12');
+    });
+}
 
